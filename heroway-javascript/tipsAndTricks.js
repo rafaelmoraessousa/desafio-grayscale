3a9a1d41d20caaa0481d640be481c2ebac97a8d1
rafaelmoraessousa <rafaelmsousa80@gmail.com>
2020-02-08 10:29:34 -0300

    Javascript Deleted cache

---
+++ heroway-javascript/tipsAndTricks.js
@@ -0,0 +1,15 @@
+const hero = {
+    id:1,
+    name: 'Spider-Man',
+    company:'Marvel'
+};
+
+showHero(
+    hero.id,
+    hero.name,
+    hero.company
+);
+
+function showHero(id, name, company){
+    console.log(showHero());
+}
